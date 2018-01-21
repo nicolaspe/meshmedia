e637201adbfe64d35e86eb99005f52b15d970b03
Nouf Aljowaysir <na1215@nyu.edu>
2018-01-20 19:12:21 -0500

    next event auto select and auto collapse when event ends

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 var clock, calen;
 var month, weekday;
-var nextEvent;
+var nextEventStart, nextEventEnd;
 var clicked, button, video;
+let data, supposedCurrentTime;
 
 window.addEventListener('load', onLoad);
 
@@ -17,6 +18,8 @@ function onLoad(){
 	weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 	$.getJSON("events.json", function(json){
+		data = json;
+		supposedCurrentTime = 0;
 		createCalendar(json);
 	});
 
@@ -33,40 +36,7 @@ function onLoad(){
 
 	// events
 	button.addEventListener('click', function(evt) {
-		if (clicked == false) {
-			let cal = document.getElementById('calendar');
-			cal.style.visibility = "hidden";
-
-			//create video
-			let src = window.location.protocol + "//" + window.location.host + "/video";
-			let ss = document.createElement("SOURCE");
-			ss.type = "video/mp4";
-			ss.src = src;
-			video.appendChild(ss);
-
-			//calculate starting point
-			let d = new Date();
-			let dif_h = d.getHours()   - nextEvent.getHours();
-			let dif_m = d.getMinutes() - nextEvent.getMinutes();
-			let dif_s = d.getSeconds() - nextEvent.getSeconds();
-			let startPoint = dif_s + dif_m*60 + dif_h*60*60;
-			video.currentTime = startPoint;
-			video.play();
-
-			document.querySelector("#video_div").appendChild(video);
-			button.innerText = "Quit Show";
-
-			clicked = true;
-
-		} else if (clicked == true) {
-			let video = document.getElementById("videoPlayer");
-			document.querySelector("#video_div").removeChild(video);
-			button.innerText = "Join Show";
-			let cal = document.getElementById('calendar');
-			cal.style.visibility = "visible";
-
-			clicked = false;
-		}
+		buttonClick();
 	});
 
 	// from https://stackoverflow.com/questions/11903545/how-to-disable-seeking-with-html5-video-tag
@@ -80,14 +50,14 @@ function onLoad(){
   		// user seeks, seeking is fired, currentTime is modified, seeking is fired, current time is modified, ....
   		var delta = video.currentTime - supposedCurrentTime;
   		if (Math.abs(delta) > 0.01) {
-    		console.log("Seeking is disabled");
+    		// console.log("Seeking is disabled");
     		video.currentTime = supposedCurrentTime;
   		}
 	});
 }
 
 function createCalendar(json){
-	let d = new Date();
+	let d1 = new Date();
 	let future = new Date();
 
 	for (let i = 0; i < 7; i++) {
@@ -95,7 +65,7 @@ function createCalendar(json){
 		let text;
 
 		// set target date
-		future.setDate(d.getDate()+i);
+		future.setDate(d1.getDate()+i);
 
 		// get target column
 		let id  = "#t" + i;
@@ -123,7 +93,7 @@ function createCalendar(json){
 		p.textContent = date;
 		col.appendChild(p);
 
-		// get events
+		// get events for that day
 		for (let j = 0; j < json.events.length; j++) {
 			if(json.events[j].date == day && json.events[j].month == mon+1 && json.events[j].year == yea){
 				// insert separation and create elements
@@ -140,18 +110,11 @@ function createCalendar(json){
 				// append data
 				col.appendChild(p);
 				col.appendChild(p2);
-				// check if it's the next event
-				let evntTime = json.events[j].start.split(":");
-				let evntDate = new Date(yea,mon,day,evntTime[0],evntTime[1]);
-				if(nextEvent == null){
-					nextEvent = evntDate;
-				} else if(nextEvent - evntDate > 0){
-					nextEvent = evntDate;
-				} else {
-				}
 			}
-		}
+		}	
 	}
+	// check next event
+	checkNextEvent();
 }
 
 function hideCal() {
@@ -189,27 +152,101 @@ function hideCal() {
 }
 
 function updateTime(){
-	let d = new Date();
+	let d2 = new Date();
 	// parse clock time
-	let yr  = d.getFullYear();
-	let mon = month[d.getMonth()];
-	let day = d.getDate();
+	let yr  = d2.getFullYear();
+	let mon = month[d2.getMonth()];
+	let day = d2.getDate();
 	if(day<10){ day = "0"+day};
 
-	let hr  = d.getHours();
+	let hr  = d2.getHours();
 	if(hr<10){ hr = "0"+hr};
-	let min = d.getMinutes();
+	let min = d2.getMinutes();
 	if(min<10){ min = "0"+min};
-	let sec = d.getSeconds();
+	let sec = d2.getSeconds();
 	if(sec<10){ sec = "0"+sec};
 
 	clock.textContent = mon +" " +day +", " +yr +" " +hr +":" +min +":" +sec;
 
 	// compare to next event to enable/disable button
 	let play_button = document.querySelector("#join");
-	if(nextEvent - d <= 0){
+	if(nextEventStart - d2 <= 0){
 		play_button.disabled = false;
+	}
+	if(nextEventEnd - d2 <= 0){
+		play_button.disabled = true;
+		buttonClick();
+		checkNextEvent();
 	}
 }
 
+function buttonClick(){
+	if (clicked == false) {
+		let cal = document.getElementById('calendar');
+		cal.style.visibility = "hidden";
 
+		//create video
+		let src = window.location.protocol + "//" + window.location.host + "/video";
+		let ss = document.createElement("SOURCE");
+		ss.type = "video/mp4";
+		ss.src = src;
+		video.appendChild(ss);
+
+		//calculate starting point
+		let d = new Date();
+		let dif_h = d.getHours()   - nextEventStart.getHours();
+		let dif_m = d.getMinutes() - nextEventStart.getMinutes();
+		let dif_s = d.getSeconds() - nextEventStart.getSeconds();
+		let startPoint = dif_s + dif_m*60 + dif_h*60*60;
+		supposedCurrentTime = startPoint;
+		video.currentTime = startPoint;
+		video.play();
+
+		document.querySelector("#video_div").appendChild(video);
+		button.innerText = "Quit Show";
+
+		clicked = true;
+
+	} else if (clicked == true) {
+		let video = document.getElementById("videoPlayer");
+		document.querySelector("#video_div").removeChild(video);
+		button.innerText = "Join Show";
+		let cal = document.getElementById('calendar');
+		cal.style.visibility = "visible";
+
+		clicked = false;
+	}
+}
+
+function checkNextEvent(){
+	let d3 = new Date();
+	// get events
+	for (let j = 0; j < data.events.length; j++) {
+		// get event date and start-end times
+		let yea = data.events[j].year;
+		let mon = data.events[j].month-1;
+		let day = data.events[j].date;
+		let evntTimeS = data.events[j].start.split(":");
+		let evntTimeE = data.events[j].end.split(":");
+		// date objects
+		let evntDateS = new Date(yea,mon,day,evntTimeS[0],evntTimeS[1]);
+		let evntDateE = new Date(yea,mon,day,evntTimeE[0],evntTimeE[1]);
+		
+		// if there's nothing defaults to that one
+		if(nextEventStart == null){
+			nextEventStart = evntDateS;
+			nextEventEnd   = evntDateE;
+		} 
+		// if the saved event has already ended and this one hasn't, replace it
+		else if(nextEventEnd - d3 < 0 && evntDateE - d3 > 0){
+			nextEventStart = evntDateS;
+			nextEventEnd   = evntDateE;
+		}
+		// if this event hasn't ended and begins before the saved one
+		else if(evntDateE - d3 > 0 && nextEventStart > evntDateS){
+			nextEventStart = evntDateS;
+			nextEventEnd   = evntDateE;
+		} else {
+		}
+	}
+}
